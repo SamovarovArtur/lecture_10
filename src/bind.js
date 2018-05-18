b20e95f6877d2eee30653b7bb5c4e52c280b1f68
art <samovarovartur@gmail.com>
2018-05-18 20:15:05 +0300

    Bind

--- src/bind.js
+++ src/bind.js
@@ -1,3 +1,21 @@
 global.bind = function (fn, context) {
-
+ var newArray = [].slice.call(arguments,2);
+    return function(){
+        return fn.apply(context, newArray);
+    }
 };
+var obj2 = {
+	shoes:'sneakers',
+	jeans:'hat',
+}
+var obj = {
+	shoes:'boots',
+	jeans:'jeans',
+	}
+var bott = function  (args) {
+	console.log('dress up ' + this.shoes + args)
+}
+var anotherBott = global.bind(bott, obj,' data');
+
+console.log(anotherBott.call(obj2))
+console.log(bott.call(obj2,' data'))
